56125dc500ef55c4a86ada3e61a9a6b724cfeb9d
Preston Hansen <preston.hansen@sproutsocial.com>
2021-01-16 10:16:01 -0600

    Make test pass.

--- client/client.go
+++ client/client.go
@@ -1,9 +1,13 @@
 package client
 
+// replace this with persistent storage
+var dictionary map[string]string = make(map[string]string)
+
 func Get(key string) string {
-	return ""
+	return dictionary[key]
 }
 
 func Put(key string, value string) error {
+	dictionary[key] = value
 	return nil
 }
